ff78f2b42b1417f31213ff23fbf61c639b548c46
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-14 16:21:39 +0300

    Append filtered list to DOM

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ const filter = document.querySelector(".filter");
 function fetchData() {
     fetch('data.json')
         .then(response => response.json())
-        .then(data => appendToDom(data))
-}
-fetchData()
+        .then(data => {
 
-// FUNCTION THAT APPENDS ITEMS TO THE DOM
-const appendToDom = (data) => {
 
-    data.map(item => {
-        const news = () => { return item.new ? `<label id="new">New</label>` : ''; }
-        const featured = () => { return item.featured ? `<label id="featured">Featured!</label>` : ''; }
+            // FUNCTION THAT APPENDS ITEMS TO THE DOM
+            const appendToDom = (data) => {
 
-        const listContainer = document.createElement('div');
-        listContainer.className = 'list-container';
-        const leftSection = document.createElement('div');
-        leftSection.className = 'leftSection';
-        leftSection.innerHTML = ` <div class="image">
+                data.map(item => {
+                    const news = () => { return item.new ? `<label id="new">New</label>` : ''; }
+                    const featured = () => { return item.featured ? `<label id="featured">Featured!</label>` : ''; }
+
+                    const listContainer = document.createElement('div');
+                    listContainer.className = 'list-container';
+                    const leftSection = document.createElement('div');
+                    leftSection.className = 'leftSection';
+                    leftSection.innerHTML = ` <div class="image">
                                       <img src="${item.logo}" alt="" srcset="">
                                      </div>
                                      <div class="jobContent">
@@ -37,72 +36,80 @@ const appendToDom = (data) => {
                                      </div>`
 
 
-        const rightSection = document.createElement('div')
-        rightSection.className = 'rightSection'
-        const button1 = document.createElement('button')
-        button1.setAttribute('id', 'role')
-        button1.innerText = item.role
-        button1.addEventListener('click', () => {
-            filterRole(item.role)
-        })
-        rightSection.appendChild(button1)
-        const button2 = document.createElement('button')
-        button2.setAttribute('id', 'level')
-        button2.innerText = item.level
-        button2.addEventListener('click', () => {
-            filterRole(item.level)
-        })
-        rightSection.appendChild(button2)
-        item.languages.map(language => {
-            const buttons = document.createElement('button')
-            buttons.setAttribute('class', 'languages')
-            buttons.innerText = language
-            buttons.addEventListener('click', () => {
-                filterRole(language)
-            })
-            rightSection.appendChild(buttons)
+                    const rightSection = document.createElement('div')
+                    rightSection.className = 'rightSection'
+                    const button1 = document.createElement('button')
+                    button1.setAttribute('id', 'role')
+                    button1.innerText = item.role
+                    button1.addEventListener('click', () => {
+                        filterRole(item.role)
+                    })
+                    rightSection.appendChild(button1)
+                    const button2 = document.createElement('button')
+                    button2.setAttribute('id', 'level')
+                    button2.innerText = item.level
+                    button2.addEventListener('click', () => {
+                        filterRole(item.level)
+                    })
+                    rightSection.appendChild(button2)
+                    item.languages.map(language => {
+                        const buttons = document.createElement('button')
+                        buttons.setAttribute('class', 'languages')
+                        buttons.innerText = language
+                        buttons.addEventListener('click', () => {
+                            filterRole(language)
+                        })
+                        rightSection.appendChild(buttons)
 
-        })
-        jobLists.appendChild(listContainer);
-        listContainer.appendChild(leftSection);
-        listContainer.appendChild(rightSection);
+                    })
+                    jobLists.appendChild(listContainer);
+                    listContainer.appendChild(leftSection);
+                    listContainer.appendChild(rightSection);
 
-    })
+                })
 
-}
+            }
 
+            appendToDom(data)
+            const filterContainer = document.createElement('div');
+            filterContainer.className = 'filter-container';
+            const btn = document.createElement('div');
+            btn.className = 'btn';
+            const clear = document.createElement('div');
+            clear.className = 'clear';
 
-const filterContainer = document.createElement('div');
-filterContainer.className = 'filter-container';
-const btn = document.createElement('div');
-btn.className = 'btn';
-const clear = document.createElement('div');
-clear.className = 'clear';
-
-// FUNCTION THAT RENDERS THE FILTER BUTTONS
-function filterRole(item) {
-    // jobLists.innerHTML = ''
-    
-    btn.innerHTML += `<div class="btn-container">
+            // FUNCTION THAT RENDERS THE FILTER BUTTONS AND FILTERED ITEMS
+            function filterRole(item) {
+                jobLists.innerHTML = ''
+                btn.innerHTML += `<div class="btn-container">
                          <button onClick="fetches"> ${item}</button><i class="fa fa-close" onClick="clearEl()"></i>
                         </div>`
-    clear.innerHTML =  ` <label for="" onClick="clears()">Clear </label>`
-   
-    filter.appendChild(filterContainer);
-    filterContainer.appendChild(btn);
-    filterContainer.appendChild(clear);
+                clear.innerHTML = ` <label for="" onClick="clears()">Clear </label>`
+                filter.appendChild(filterContainer);
+                filterContainer.appendChild(btn);
+                filterContainer.appendChild(clear);
+                
+                // FILTERS DATA DEPENDING ON THE SELECTRED BUTTON AND RENDERS TO DOM
+                const fil = data.filter(items => {
+                    const lan = items.languages.map(language => language).split(',');
+                    return items.role === item || items.level === item || lan === item ? items : null
+                })
+                console.log(fil)
+                appendToDom(fil)
+                console.log(item)
+            }
+          
 
+        })
+      
 }
+fetchData()
 
-// FUNCTION TO CLEAR 
 const clears = () => {
     btn.innerHTML = '';
     filterContainer.remove();
 }
-const clearEl = () => {
-console.log(btn)
 
-}
 
 
 
